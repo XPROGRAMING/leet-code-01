56e772bd445225cf7cc5c6d0d431763abedeaaa2
Michael Henrique <michaeltraderwiner@gmail.com>
2026-05-25 16:56:09 -0300

    exrcicios

--- main.py
+++ main.py
@@ -27,3 +27,48 @@ vendas = [
 
 # for nome, total in vendedores.items():
 #     print(nome, "vendeu:", total)
+
+
+# exercio 03
+# vendedores = {}
+
+# for venda in vendas:
+#     nome = venda["vendedor"]
+#     valor = venda["valor"]
+
+#     if nome not in vendedores:
+#         vendedores[nome] = 0
+
+#     vendedores[nome] += valor
+
+# maior_valor = 0
+# melhor_vendedor = ""
+
+# for nome, total in vendedores.items():
+
+#     if total > maior_valor:
+#         maior_valor = total
+#         melhor_vendedor = nome
+# print("Maior vendedor:", melhor_vendedor)
+# print("Total vendido:", maior_valor)
+
+produtos = {}
+
+for venda in vendas:
+    produto = venda["produto"]
+    valor = venda["valor"]
+   
+    if produto not in produtos:
+        produtos[produto] = 0
+
+
+print(produtos)
+
+vendas = [
+    {"produto": "Notebook", "valor": 3500, "vendedor": "Ana"},
+    {"produto": "Mouse", "valor": 80, "vendedor": "Carlos"},
+    {"produto": "Teclado", "valor": 150, "vendedor": "Ana"},
+    {"produto": "Monitor", "valor": 900, "vendedor": "Carlos"},
+    {"produto": "Notebook", "valor": 4000, "vendedor": "João"},
+    {"produto": "Mouse", "valor": 100, "vendedor": "Ana"},
+]
---
+++ teste.py
@@ -0,0 +1,163 @@
+# exercicio 01
+# nums = [10,20,30,40,50]
+
+# soma = 0
+# for num in nums:
+#     soma += num
+# print(soma)
+
+
+# exercicio 02
+# nums = [4,8,1,99,23]
+
+# maior = 0
+# for i in nums:
+#     if i > maior:
+#         maior = i
+
+# print (maior)
+
+# exercicio 03
+
+# nums = [2,5,8,11,14,20]
+
+# par = []
+# for num in nums:
+#     if num % 2 == 0:
+#         par.append(num)
+
+# print(par)
+
+# exercicio 04
+
+# nums = [10,20,30]
+
+# soma = 0
+# for num in nums:
+#     soma += num
+
+# tamanho = len(nums)
+# media = soma / tamanho
+
+# print("A media e: ", media)
+
+# exercicio 05
+# nums = [1,1,2,2,3,4,4]
+
+# noduplicatec = []
+# for num in nums:
+#     if num not in noduplicatec:
+#         noduplicatec.append(num)
+
+# print(noduplicatec)
+
+# exercicio 06
+# nums = [5,12,7,20,3,15]
+# m10 = []
+
+# for num in nums:
+#     if num > 10:
+#         m10.append(num)
+
+# print(m10)
+
+# exercicio 07
+# nums = [1,2,3,4]
+# lista_invertida = nums[::-1]
+
+# print(lista_invertida)
+
+#exercicio 08
+# palavras = ["python", "java", "python", "c", "java", "python"]
+# somatoria = {}
+
+# for palavra in palavras:
+#     if palavra not in somatoria:
+#         somatoria[palavra] = 0
+#     somatoria[palavra] += 1
+
+
+# print(somatoria)
+
+#exercicio 09
+
+# notas = {
+#     "Ana": 8,
+#     "Carlos": 10,
+#     "João": 7
+# }
+# maior_nota = 0
+# nome01 = ""
+# for nome, nota in notas.items():
+#     if nota > maior_nota:
+#         maior_nota = nota
+#         nome01 = nome
+
+# print("A maior nota e do: ", nome01, "que tirou", maior_nota)
+
+#exercicio 10
+
+# gastos = [
+#     {"categoria": "comida", "valor": 50},
+#     {"categoria": "transporte", "valor": 30},
+#     {"categoria": "comida", "valor": 70},
+# ]
+
+# soma ={}
+
+# for gasto in gastos:
+#     categoria = gasto["categoria"]
+#     valor = gasto["valor"]
+
+#     if categoria not in soma:
+#         soma[categoria] = 0
+#     soma[categoria] += valor
+
+# print(soma)
+
+# exercicio 11
+# 11-a. Some as vendas por vendedor
+# vendas = [
+#     {"vendedor": "Ana", "valor": 300},
+#     {"vendedor": "Carlos", "valor": 500},
+#     {"vendedor": "Ana", "valor": 700},
+#     {"vendedor": "João", "valor": 400},
+#     {"vendedor": "Carlos", "valor": 200}
+# ]
+# somatoria = {}
+
+# for venda in vendas:
+#     vendedor = venda["vendedor"]
+#     valor = venda["valor"]
+    
+#     if vendedor not in somatoria:
+#         somatoria[vendedor] = 0
+#     somatoria[vendedor] += valor
+# print(somatoria)
+
+#11-b Descubra quem vendeu mais
+vendas = [
+    {"vendedor": "Ana", "valor": 300},
+    {"vendedor": "Carlos", "valor": 500},
+    {"vendedor": "Ana", "valor": 700},
+    {"vendedor": "João", "valor": 400},
+    {"vendedor": "Carlos", "valor": 200}
+]
+somatoria = {}
+
+for venda in vendas:
+    vendedor = venda["vendedor"]
+    valor = venda["valor"]
+    
+    if vendedor not in somatoria:
+        somatoria[vendedor] = 0
+    somatoria[vendedor] += valor
+
+maior_vendedor = 0
+nome01 = ""
+for nome, valor in somatoria.items():
+    if valor > maior_vendedor:
+        maior_vendedor = valor
+        nome01 = nome
+
+print("o maior vendedor foi: ", nome01, "Que vendeu: ", maior_vendedor)
